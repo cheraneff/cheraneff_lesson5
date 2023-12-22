28822f0fe6b932a3392e9f8e873c7ecfb95bfb14
Дом <@DESKTOP-VTH5MV3>
2023-12-22 10:53:18 +0500

    Добавьте файлы проекта.

---
+++ Level.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Level
+    {
+
+        public Level()
+        {
+           
+        }
+
+        //================================= Fields ========================
+        #region Fields
+
+        /// <summary>
+        /// Цена уровня
+        /// </summary>
+        public decimal PriceLevel = 0;
+
+        /// <summary>
+        /// Лот на уровень
+        /// </summary>
+        public decimal LotLevel = 1;
+
+        /// <summary>
+        /// Открытый объем на уровень
+        /// </summary>
+        public decimal Volume = 0;
+
+
+
+        #endregion
+
+
+    }
+}
